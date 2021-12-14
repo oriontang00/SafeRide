77e64f3df1dfc8fbdc72e4495db30265185bc675
doubleapple123 <leonychen123@gmail.com>
2021-12-13 18:16:27 -0800

    update

--- SafeRide/SafeRide/src/Security/UserSecurity/UserSecurity.cs
+++ SafeRide/SafeRide/src/Security/UserSecurity/UserSecurity.cs
@@ -1,17 +1,37 @@
-﻿using SafeRide.src.Security.Interfaces;
+﻿using SafeRide.src.Interfaces;
+using SafeRide.src.Models;
+using SafeRide.src.Security.Interfaces;
 
 namespace SafeRide.src.Security
 {
     public class UserSecurity : ISecurity
     {
+        private User user;
+
+        public UserSecurity(User user)
+        {
+            this.user = user;
+            bool isAuthenticate = Authenticate(UserAuthenticate, user);
+            bool isAuthorized = Authorize(UserAuthorize, user);
+        }
+        private bool UserAuthenticate(object user)
+        {
+            return user != null;
+        }
+
+        private bool UserAuthorize(object user)
+        {
+            return user != null;
+        }
+
         public bool Authenticate(Func<object, bool> func, object obj)
         {
-            throw new NotImplementedException();
+            return func(obj);
         }
 
         public bool Authorize(Func<object, bool> func, object obj)
         {
-            throw new NotImplementedException();
+            return func(obj);
         }
     }
 }
