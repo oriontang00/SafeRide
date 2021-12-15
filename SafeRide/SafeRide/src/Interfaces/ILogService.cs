c9379e51ca190dc678cc82359bc8159ea13678b5
oriontang00 <oriontang00@gmail.com>
2021-12-14 17:21:59 -0800

    Added Logging Files

---
+++ SafeRide/SafeRide/src/Interfaces/ILogService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SafeRide.src.Models;
+
+namespace SafeRide.src.Interfaces
+{
+    interface ILogService
+    {
+        public bool Write(LogMessage message);
+    }
+}
