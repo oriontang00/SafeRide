0db0c2ec48033987878ffe16228b9f68aee7beed
oriontang00 <oriontang00@gmail.com>
2021-12-15 03:43:09 -0800

    Add comments to Logging components

--- SafeRide/SafeRide/src/Logging/DBLogService.cs
+++ SafeRide/SafeRide/src/Logging/DBLogService.cs
@@ -8,15 +8,26 @@ using SafeRide.src.Models;
 
 namespace SafeRide.src.Logging
 {
+    /// <summary>
+    /// This class defines functionality to log messages from anywhere within the system.
+    /// </summary>
     public class DBLogService : ILogService
     {
         private ILogMessageDAO _logMessageDAO;
 
+       /// <summary>
+       /// Constructor which instantiates an ILogMessageDAO object to let this class communicate with the database.
+       /// </summary>
         public DBLogService()
         {
             _logMessageDAO = new LogMessageSQLServerDAO();
         }
 
+        /// <summary>
+        /// Passes the log message to be logged to the data access object to insert to database.
+        /// </summary>
+        /// <param name="message">The message to be logged</param>
+        /// <returns>A boolean value representing success status of inserting message into database</returns>
         public bool Write(LogMessage message)
         {
 
