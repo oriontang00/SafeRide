36f2dc757dd0d4159e436bd90476a7529ecd008e
oriontang00 <oriontang00@gmail.com>
2021-12-14 21:16:33 -0800

    Completed logging component

--- SafeRide/SafeRide/Program.cs
+++ SafeRide/SafeRide/Program.cs
@@ -1,8 +1,39 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using SafeRide.src.Logging;
+using SafeRide.src.Interfaces;
+using SafeRide.src.DataAccess;
+using SafeRide.src.Models;
 
-Console.WriteLine("Testing string");
+ILogService logService = new DBLogService();
+ILogMessageDAO logDAO = new LogMessageSQLServerDAO();
 
-app.MapGet("/", () => "Hello World!");
+LogMessage message1 = new LogMessage("this is a test log", Level.Information);
+LogMessage message2 = new LogMessage("this is a test log", Level.Debug);
 
-app.Run();
+Console.WriteLine(logService.Write(message1));
+Console.WriteLine(logService.Write(message2));
+
+List<LogMessage> logList = new List<LogMessage>();
+
+DateTime start = new DateTime(2021, 12, 14, 19, 15, 0);
+DateTime end = new DateTime(2021, 12, 14, 20, 0, 0);
+
+logList = logDAO.GetAllLogs();
+
+foreach (LogMessage log in logList)
+{
+    Console.WriteLine(log + "\n");
+}
+
+Console.WriteLine("Deleting Logs between 7:15 and 8:00 ....\n");
+
+logDAO.DeleteByTimeRange(start, end);
+
+logList = logDAO.GetAllLogs();
+
+foreach (LogMessage log in logList)
+{
+    Console.WriteLine(log + "\n");
+}
+
+
+
