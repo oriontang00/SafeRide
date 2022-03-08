8ccb38ebff453939c1cb65df078eb9e9989d743d
Orion Tang <64755901+oriontang00@users.noreply.github.com>
2022-03-07 17:40:25 -0800

    Create populatetables.sql

---
+++ SQLQueries/populatetables.sql
@@ -0,0 +1,19 @@
+INSERT INTO Users (firstName, lastName, userName, userID, phoneNum, password, isAdmin, enabled, lastLogin, dateRegistered) VALUES('Leon', 'Chen', 'AppleUser', 'appleID123', '0011', 'applepw','1', '1', '20111114 00:00:00 AM', '20211214 00:00:00 AM'); 
+
+INSERT INTO Users (firstName, lastName, userName, userID, phoneNum, password, isAdmin, enabled, lastLogin, dateRegistered) VALUES
+('Orion', 'Tang', 'OrionTang', '1246534', '5346', 'orionpw','1', '1', '20220304 00:00:00 AM', '20220223 00:00:00 AM'),
+('Andy', 'Lee', 'Andyuser', 'a463456', '3456', 'andypw','1', '1', '20220303 00:00:00 AM', '20211014 00:00:00 AM'); 
+
+INSERT INTO Users (firstName, lastName, userName, userID, phoneNum, password, isAdmin, enabled, lastLogin, dateRegistered) VALUES
+('bubba', 'lee', 'asda', 'eqwreq', '4325', 'bubapw','1', '1', '20220304 00:00:00 AM', '20220223 00:00:00 AM'),
+('colin', 'creasman', 'colinuser', 'dgdf', '3545', 'colinpw','1', '1', '20220303 00:00:00 AM', '20220214 00:00:00 AM'); 
+
+INSERT INTO ViewEvents (userID, viewURL, startDate, endDate, elapsedSeconds) VALUES
+('id1', 'saferide.com/routehistory', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 500),
+('id2', 'saferide.com/routehistory', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 700),
+('id3', 'saferide.com/login', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 200),
+('id4', 'saferide.com/login', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 200),
+('id5', 'saferide.com/map', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 5000),
+('id6', 'saferide.com/shareroute', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 300),
+('id7', 'saferide.com/accountsettings', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 550),
+('id8', 'saferide.com/epicpoggers', '20220307 00:00:00 AM', '20220307 00:00:00 AM', 100);
