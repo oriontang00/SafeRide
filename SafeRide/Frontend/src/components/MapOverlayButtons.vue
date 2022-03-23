9b2a3a9d22b71e61e29f4d53f0d6c049492eb9a7
Leon <leonychen123@gmail.com>
2022-03-23 16:49:53 -0700

    update

---
+++ SafeRide/Frontend/src/components/MapOverlayButtons.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="overlayButtons" v-for="overlay in possibleOverlays">
+    <Overlay overlay={{overlay}}></Overlay>
+  </div>
+</template>
+
+<script>
+import Overlay from "@/components/Overlay.vue"
+export default {
+  components: {
+    Overlay
+  },
+  name: "MapOverlayButtons",
+  props:{
+    possibleOverlays: {
+      type: Array
+    }
+  },
+  methods:{
+    getOverLays () {
+      this.possibleOverlays = ["test1", "test2", "test3"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ SafeRide/Frontend/src/components/Overlay.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="singleOverlay">
+    <button>{{overlay.overLayName}}</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Overlay",
+  props:{
+    overlay:{
+      type: Object,
+      overLayName: String
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
